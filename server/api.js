eea5ba108c20640d4c93e4b9e6bd4738b0159086
mlacosta <marianoacosta.003@gmail.com>
2020-07-22 13:03:47 -0300

    implemented use in router

--- server/api.js
+++ server/api.js
@@ -75,37 +75,21 @@ const deleteItem = (req,res)=>{
 // Minion Router
 const minionsRouter = express.Router({mergeParams: true});
 
-minionsRouter.get('/', setName('minions'));
+minionsRouter.use(setName('minions'));
 minionsRouter.get('/', getAllItem);
-
-minionsRouter.post('/', setName('minions'));
 minionsRouter.post('/', postItem);
-
-minionsRouter.get('/:minionId', setName('minions'));
 minionsRouter.post('/', getItem);
-
-minionsRouter.put('/:minionId',setName('minions'));
 minionsRouter.put('/:minionId',putItem);
-
-minionsRouter.delete('/:minionId',setName('minions'));
 minionsRouter.delete('/:minionId',deleteItem);
 
 // Ideas Router
 const ideasRouter = express.Router({mergeParams: true});
 
-ideasRouter.get('/', setName('ideas'));
+ideasRouter.use(setName('ideas'));
 ideasRouter.get('/', getAllItem);
-
-ideasRouter.post('/', setName('ideas'));
 ideasRouter.post('/', postItem);
-
-ideasRouter.get('/:ideaId', setName('ideas'));
 ideasRouter.post('/', getItem);
-
-ideasRouter.put('/:ideaId',setName('ideas'));
 ideasRouter.put('/:ideaId',putItem);
-
-ideasRouter.delete('/:ideaId',setName('ideas'));
 ideasRouter.delete('/:ideaId',deleteItem);
 
 // Meetings Router
